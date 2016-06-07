8aeaba08dc4888e3406eb4bb88664e96a72d4c7e
mbr4nt <mateus.brant@gmail.com>
2016-06-07 11:03:33 -0500

    Update patternSplitter.js

--- patternSplitter.js
+++ patternSplitter.js
@@ -64,7 +64,7 @@ module.exports = function(path, pattern, groupPattern, eachBlockCallback, doneCa
             if (currentChildBlock) {
                 currentChildBlock[match[1]] = match[2];
             }
-            else {
+            else if(currentBlock) {
                 currentBlock[match[1]] = match[2];
             }
         }
